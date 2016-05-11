10e01c2c8dc1847eafc23fd5da1e663da25710b5
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-05-11 21:17:47 +0200

    crypton

---
+++ misc/crypton/cryptonRd.R
@@ -0,0 +1,10 @@
+#' Datasets used in the crypton game.
+#'
+#' Datasets used in the crypton game.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name birthdays
+#' @aliases birthdays messages user_ids
+NULL
+
